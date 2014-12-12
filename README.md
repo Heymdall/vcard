8b61fa9e70948a75f8efb1f177106a1c3311b79d
Aleksandr Kitov <kitov.nn@gmail.com>
2014-12-12 17:18:07 +0300

    Add spec for other vCard versions, little fixes

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 vcard
 =====
 
-Simple js vCard parser
+Simple js vCard parser/generator.
 
 Example usage:
 ```javascript
+var rawCard = 'BEGIN:VCARD\n' +
+               'VERSION:3.0\n' +
+               'N:;Alex;;;\n' +
+               'UID:3a43f748-050f-40a3-a47f-9aab0cf9b36e@178.62.218.194\n' +
+               'REV:2014-12-10T14:15:16+00:00\n' +
+               'FN:Alex\n' +
+               'PRODID:-//ownCloud//NONSGML Contacts 0.3.0.18//EN\n' +
+               'ORG:ASD\n' +
+               'TEL;TYPE=HOME:12345678900\n' +
+               'BDAY;VALUE=DATE:2014-12-01\n' +
+               'END:VCARD\n'';
+
 var card = VCard.parse(rawCard);
 card.properties.org = 'GitHub';
 rawCard = card.getCard();
--- src/vcard.js
+++ src/vcard.js
@@ -1,90 +1,89 @@
 (function (namespace) {
-  function VCard(fields) {
-    this.properties = {};
-    Object.keys(fields).forEach(function (prop) {
-      this.properties[prop] = fields[prop];
-    }.bind(this));
-  }
-
-  /**
-   * Возвращает VCard представление
-   * @returns {string}
-   */
-  VCard.prototype.getCard = function () {
-    var prefix = 'BEGIN:VCARD',
-      postfix = 'END:VCARD',
-      lines = [];
-
-    lines.push(prefix);
-
-    for (var prop in this.properties) {
-      if (typeof this.properties[prop] === 'string') {
-        lines.push(prop.toUpperCase() + ':' + this.properties[prop]);
-      } else {
-        this.properties[prop].forEach(function (property) {
-          var meta = [];
-          for (var metaName in property.meta) {
-            meta.push(metaName.toUpperCase() + '=' + property.meta[metaName].toUpperCase());
-          }
-          lines.push(prop.toUpperCase() + ';' + meta.join(';') + ':' + property.value.join(';'));
-        });
-      }
+    function VCard(fields) {
+        Object.keys(fields).forEach(function (prop) {
+            this[prop] = fields[prop];
+        }.bind(this));
     }
 
-    lines.push(postfix);
+    /**
+     * Возвращает VCard представление
+     * @returns {string}
+     */
+    VCard.prototype.getCard = function () {
+        var prefix = 'BEGIN:VCARD',
+            postfix = 'END:VCARD',
+            lines = [];
 
-    return lines.join("\n").trim();
-  };
+        lines.push(prefix);
 
-  /**
-   * Разбирает VCard представление
-   * @param {string} input Строка VCard
-   * @param {string} url
-   * @returns {VCard}
-   */
-  VCard.parse = function (input, url) {
-    var Re1 = /^(version|fn|title|org|nickname|note|uid|n|rev|prodid):(.+)$/i;
-    var Re2 = /^([^:;]+);([^:]+):(.+)$/;
-    var ReKey = /item\d{1,2}\./;
-    var fields = {};
-
-    input.split(/\r\n|\r|\n/).forEach(function (line) {
-      var results, key;
-
-      if (Re1.test(line)) {
-        results = line.match(Re1);
-        key = results[1].toLowerCase().trim();
-        fields[key] = results[2];
-      } else if (Re2.test(line)) {
-        results = line.match(Re2);
-        key = results[1].replace(ReKey, '').toLowerCase().trim();
-
-        var meta = {};
-        results[2].split(';')
-          .map(function (p, i) {
-            var match = p.match(/([a-z\-]+)=(.*)/i);
-            if (match) {
-              return [match[1], match[2]];
-            } else {
-              return ['TYPE' + (i === 0 ? '' : i), p];
+        for (var prop in this) {
+            if (typeof this[prop] === 'string') {
+                lines.push(prop.toUpperCase() + ':' + this[prop]);
+            } else if (typeof this[prop] !== 'function') {
+                this[prop].forEach(function (property) {
+                    var meta = [];
+                    for (var metaName in property.meta) {
+                        meta.push(metaName.toUpperCase() + '=' + property.meta[metaName].toUpperCase());
+                    }
+                    lines.push(prop.toUpperCase() + ';' + meta.join(';') + ':' + property.value.join(';'));
+                });
             }
-          })
-          .forEach(function (p) {
-            meta[p[0].toLowerCase()] = p[1].toLowerCase();
-          });
+        }
 
-        if (!fields[key]) fields[key] = [];
+        lines.push(postfix);
 
-        fields[key].push({
-          meta: meta,
-          value: results[3].split(';')
+        return lines.join("\n").trim();
+    };
+
+    /**
+     * Разбирает VCard представление
+     * @param {string} input Строка VCard
+     * @param {string} url
+     * @returns {VCard}
+     */
+    VCard.parse = function (input, url) {
+        var Re1 = /^(version|fn|title|org|nickname|note|uid|n|rev|prodid):(.+)$/i;
+        var Re2 = /^([^:;]+);([^:]+):(.+)$/;
+        var ReKey = /item\d{1,2}\./;
+        var fields = {};
+
+        input.split(/\r\n|\r|\n/).forEach(function (line) {
+            var results, key;
+
+            if (Re1.test(line)) {
+                results = line.match(Re1);
+                key = results[1].toLowerCase().trim();
+                fields[key] = results[2];
+            } else if (Re2.test(line)) {
+                results = line.match(Re2);
+                key = results[1].replace(ReKey, '').toLowerCase().trim();
+
+                var meta = {};
+                results[2].split(';')
+                    .map(function (p, i) {
+                        var match = p.match(/([a-z\-]+)=(.*)/i);
+                        if (match) {
+                            return [match[1], match[2]];
+                        } else {
+                            return ['TYPE' + (i === 0 ? '' : i), p];
+                        }
+                    })
+                    .forEach(function (p) {
+                        meta[p[0].toLowerCase()] = p[1].toLowerCase();
+                    });
+
+                if (!fields[key]) fields[key] = [];
+
+                fields[key].push({
+                    meta: meta,
+                    value: results[3].split(';')
+                });
+            }
         });
-      }
-    });
 
-    return new VCard(fields, url);
-  };
+        return new VCard(fields, url);
+    };
 
 
-  namespace.VCard = VCard;
+    namespace.VCard = VCard;
 })(typeof window !== 'undefined' ? window : module.exports);
